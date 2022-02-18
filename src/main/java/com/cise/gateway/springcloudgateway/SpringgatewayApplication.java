b367f02c5664353f1571174169babb6a8e2e6b89
enextus <enextus@gmail.com>
2022-02-18 04:12:08 +0100

    changes

---
+++ src/main/java/com/cise/gateway/springcloudgateway/SpringgatewayApplication.java
@@ -0,0 +1,26 @@
+package com.cise.gateway.springcloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringgatewayApplication {
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/get")
+						.filters(f -> f.addRequestHeader("Hello1", "World1"))
+						.uri("http://httpbin.org:80"))
+				.build();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringgatewayApplication.class, args);
+	}
+
+}
